897b1e596aa16432ad93b070dda2a78e1d4e114c
liyong <liyong@xnw.com>
2019-02-18 10:26:52 +0800

    Initial Commit

---
+++ LYVideoPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYVideoPlayer
+//
+//  Created by LiYong on 2019/2/18.
+//  Copyright © 2019 勇 李. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYVideoPlayer/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LYVideoPlayer
+//
+//  Created by LiYong on 2019/2/18.
+//  Copyright © 2019 勇 李. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
